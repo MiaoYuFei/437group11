63de3d2a8e953172bb847178d9acec1560359e11
Ruowen Xu <ruowen@wustl.edu>
2023-02-21 18:07:44 -0600

    login

--- frontend/stocknews/src/views/LoginView.vue
+++ frontend/stocknews/src/views/LoginView.vue
@@ -8,6 +8,22 @@ export default {
 };
 </script>
 <template>
-  <div>Log in page for stock news</div>
+  <div>Log in page for stock news
+    <form action="http://localhost:9001/api/login" method="post">
+            <h3>Sign In</h3>
+
+            <div>
+                <label>user name</label>
+                <input name="username" type="text">
+            </div>
+            <div>
+                <label>Password</label>
+                <input name="password" type="password">
+            </div>
+            <button type="submit" class="btn btn-dark btn-lg btn-block">Sign In</button>
+
+    </form>  
+  </div>
+  
 </template>
 <style scoped></style>

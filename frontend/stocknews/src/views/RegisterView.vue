61b44794571719eab62dfa80a49a18bc40a62ad5
Ruowen Xu <ruowen@wustl.edu>
2023-02-21 18:17:06 -0600

    register

--- frontend/stocknews/src/views/RegisterView.vue
+++ frontend/stocknews/src/views/RegisterView.vue
@@ -8,6 +8,26 @@ export default {
 };
 </script>
 <template>
-  <div>Register page for stock news</div>
+  <div>Register page for stock news
+    <form action="http://localhost:9001/api/register" method="post">
+            <h3>Sign Up</h3>
+
+            <div>
+                <label>User Name</label>
+                <input name="username" type="text">
+            </div>
+            <div>
+                <label>Password</label>
+                <input name="password" type="password">
+            </div>
+             <div>
+                <label>Email</label>
+                <input name="email" type="email">
+            </div>           
+            <button type="submit" class="btn btn-dark btn-lg btn-block">Sign Up</button>
+
+    </form>  
+  </div>
+  
 </template>
 <style scoped></style>

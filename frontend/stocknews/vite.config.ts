16aaa8eebec403fbd7c456ad50bdbb29b5a83e03
Yufei Miao <yufeim@yufeim.com>
2023-02-27 14:14:11 -0600

    Revert "change api port"
    This reverts commit 0a708483179d99a6bd50c1af6f9b425343aaf1aa.

--- frontend/stocknews/vite.config.ts
+++ frontend/stocknews/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     host: true,
     proxy: {
       "/api": {
-        target: "http://localhost:8080/",
+        target: "http://localhost:9000/",
         changeOrigin: true,
       },
     },

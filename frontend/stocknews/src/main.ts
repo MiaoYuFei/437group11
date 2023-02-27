4421513a06c2698a388d5233a0ac24c99f53d699
Yufei Miao <yufeim@yufeim.com>
2023-02-27 14:13:57 -0600

    Revert "main"
    This reverts commit fd91d8cba851ec9090c848cbb623c4336e07e7b4.

--- frontend/stocknews/src/main.ts
+++ frontend/stocknews/src/main.ts
@@ -1,9 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css'
 
-import App from './App.vue' ;
+import App from "./App.vue";
 import router from "./router";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -13,6 +11,8 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
+app.config.globalProperties.$projectName = "Stock news";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
@@ -24,4 +24,4 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 library.add(faCircleExclamation, faHome, faMagnifyingGlass, faUser, faUserPlus);
 
-app.use(Antd).mount("#app");
+app.mount("#app");

728a60596d315f452adbbe4ba549fbe9efa5aeb3
Ruowen Xu <ruowen@wustl.edu>
2023-02-28 16:54:12 -0600

    profile

--- frontend/stocknews/src/views/ProfileView.vue
+++ frontend/stocknews/src/views/ProfileView.vue
@@ -1,64 +1,101 @@
-import { UserOutlined, VideoCameraOutlined, UploadOutlined } from '@ant-design/icons-vue';
-import { defineComponent, ref } from 'vue';
+ <script lang="ts">
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import BsAlert from "@/components/BsAlert.vue";
+import BsButton from "@/components/BsButton.vue";
+import { disableForm, enableForm, focusForm, handleApi } from "@/utilities";
 
-<script setup lang="ts"></script>
-<script lang="ts">
 export default {
   data() {
-    return {};
+    return{
+      userid:"",
+      username:"",
+      email:"",
+      emailVerified:"",
+    }
   },
-  created() {
-    document.title = "Profile - " + (this as any).$projectName;
+  watch: {
+    loading(newValue) {
+      const formAlert = this.$refs.formAlert as typeof BsAlert;
+      if (newValue) {
+        formAlert.hide();
+        disableForm(this.$refs.form);
+      } else {
+        formAlert.show();
+        enableForm(this.$refs.form);
+      }
+    },
   },
-  mounted() {},
+ methods:{
+            update(){
+                  handleApi(this.$refs.form, []).then(doc => {
+                          var code = doc.data.code;
+                          var data = doc.data.data;
+                      if (parseInt(code) == 200){
+                        this.userid = data.id;
+                        this.username = data.username;
+                        this.email = data.email;
+                        this.emailVerified = data.emailVerified;
+                      }   
+                  })
+            },
+        },
+  mounted() {
+    this.update()
+  },
+  components: {
+    FontAwesomeIcon,
+    BsAlert,
+    BsButton,
+  },
 };
 </script>
 <template>
-  <a-layout>
-    <a-layout-sider
-      breakpoint="lg"
-      collapsed-width="0"
-      @collapse="onCollapse"
-      @breakpoint="onBreakpoint"
-    >
-      <div class="logo" />
-      <a-menu  theme="dark" mode="inline">
-        <a-menu-item key="My Data">
-          <video-camera-outlined />
-          <span class="nav-text">My Data</span>
-        </a-menu-item>
-      </a-menu>
-    </a-layout-sider>
-    <a-layout>
-      <a-layout-header :style="{ background: '#fff', padding: 0 }" />
-      <a-layout-content style="text-align: center">
-        <div :style="{ padding: '10px', background: '#fff', minHeight: '360px' }">User Information</div>
-      </a-layout-content>
-      <a-layout-footer style="text-align: center">
-        Created by Group 11 for 437
-      </a-layout-footer>
-    </a-layout>
-  </a-layout>
+<div class="page-content page-container" id="page-content">
+  <link href="/Users/xuruowen/Documents/437group11-main/frontend/stocknews/src/views/trt.css" rel="stylesheet" type="text/css"/>
+    <div class="padding">
+        <div class="row container d-flex justify-content-center">
+            <div class="col-xl-6 col-md-12">
+                      <div class="card user-card-full">
+                          <div class="row m-l-0 m-r-0">
+                              <div class="col-sm-4 bg-c-lite-green user-profile">
+                                  <div class="card-block text-center text-white">
+                                      <div class="m-b-25">
+                                        <h2><FontAwesomeIcon icon="fa-user" class="me-2" /></h2>
+                                      </div>
+                                      <h3 class="f-w-600">{{username}}</h3>                                 
+                                       </div>
+                              </div>
+                              <div class="col-sm-8">
+                                  <div class="card-block">
+                                      <h3 class="m-b-20 p-b-5 b-b-default f-w-600">User Information</h3>
+                                      <div class="row">
+                                          <div class="col-sm-6">
+                                              <p class="m-b-10 f-w-600">Email</p>
+                                              <h6 class="text-muted f-w-400">{{email}}</h6>
+                                          </div>
+                                          <div class="col-sm-6">
+                                               <p class="m-b-10 f-w-600">Verified</p>
+                                               <h6 class="text-muted f-w-400">{{emailVerified}}</h6>
+                                          </div>
+                                      </div>
+                                     <h5 class="m-b-20 m-t-40 p-b-5 b-b-default f-w-600"> </h5>
+                                      <div class="row">
+                                          <div class="col-sm-6">
+                                              <p class="m-b-10 f-w-600"> </p>
+                                              <h6 class="text-muted f-w-400"></h6>
+                                          </div>
+                                      </div>
+                                      <ul class="social-link list-unstyled m-t-40 m-b-10">
+                                          <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="facebook" data-abc="true"><i class="mdi mdi-facebook feather icon-facebook facebook" aria-hidden="true"></i></a></li>
+                                          <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="twitter" data-abc="true"><i class="mdi mdi-twitter feather icon-twitter twitter" aria-hidden="true"></i></a></li>
+                                          <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="instagram" data-abc="true"><i class="mdi mdi-instagram feather icon-instagram instagram" aria-hidden="true"></i></a></li>
+                                      </ul>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+          </div>
+        </div>
+    </div>
 </template>
-<script>
-
-
-</script>
-<style>
-#components-layout-demo-top-side .logo {
-  float: left;
-  width: 120px;
-  height: 60px;
-  margin: 16px 24px 16px 0;
-  background: rgba(255, 255, 255, 0.3);
-}
-
-.ant-row-rtl #components-layout-demo-top-side .logo {
-  float: right;
-  margin: 16px 0 16px 24px;
-}
-
-.site-layout-background {
-  background: #fff;
-}
-</style>

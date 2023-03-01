68e9b101ad3db276f60c36590b3fe8cd5ea9a3af
Ruowen Xu <ruowen@wustl.edu>
2023-02-28 18:52:39 -0600

    adjust

--- frontend/stocknews/src/views/ProfileView.vue
+++ frontend/stocknews/src/views/ProfileView.vue
@@ -10,7 +10,6 @@ export default {
       userid:"",
       username:"",
       email:"",
-      emailVerified:"",
     }
   },
   watch: {
@@ -34,7 +33,6 @@ export default {
                         this.userid = data.id;
                         this.username = data.username;
                         this.email = data.email;
-                        this.emailVerified = data.emailVerified;
                       }   
                   })
             },
@@ -51,7 +49,6 @@ export default {
 </script>
 <template>
 <div class="page-content page-container" id="page-content">
-  <link href="/Users/xuruowen/Documents/437group11-main/frontend/stocknews/src/views/trt.css" rel="stylesheet" type="text/css"/>
     <div class="padding">
         <div class="row container d-flex justify-content-center">
             <div class="col-xl-6 col-md-12">
@@ -60,7 +57,7 @@ export default {
                               <div class="col-sm-4 bg-c-lite-green user-profile">
                                   <div class="card-block text-center text-white">
                                       <div class="m-b-25">
-                                        <h2><FontAwesomeIcon icon="fa-user" class="me-2" /></h2>
+                                        <h1><FontAwesomeIcon icon="fa-user" class="me-2" /></h1>
                                       </div>
                                       <h3 class="f-w-600">{{username}}</h3>                                 
                                        </div>
@@ -73,12 +70,16 @@ export default {
                                               <p class="m-b-10 f-w-600">Email</p>
                                               <h6 class="text-muted f-w-400">{{email}}</h6>
                                           </div>
+                                          <div class="col-sm-6">
+                                               <p class="m-b-10 f-w-600">Verified</p>
+                                               <h6 class="text-muted f-w-400">True</h6>
+                                          </div>
                                       </div>
-                                     <h5 class="m-b-20 m-t-40 p-b-5 b-b-default f-w-600"> </h5>
+                                      <h5 class="m-b-20 m-t-40 p-b-5 b-b-default f-w-600">Preference</h5>
                                       <div class="row">
                                           <div class="col-sm-6">
-                                              <p class="m-b-10 f-w-600"> </p>
-                                              <h6 class="text-muted f-w-400"></h6>
+                                              <p class="m-b-10 f-w-600">Recent</p>
+                                              <h6 class="text-muted f-w-400">Economy</h6>
                                           </div>
                                       </div>
                                       <ul class="social-link list-unstyled m-t-40 m-b-10">
@@ -95,3 +96,143 @@ export default {
         </div>
     </div>
 </template>
+<style>
+body {
+    background-color: #f9f9fa
+}
+
+.padding {
+    padding: 3rem !important
+}
+
+.user-card-full {
+    overflow: hidden;
+}
+
+.card {
+    border-radius: 6px;
+    -webkit-box-shadow: 0 1px 20px 0 rgba(69,90,100,0.08);
+    box-shadow: 0 1px 20px 0 rgba(69,90,100,0.08);
+    border: none;
+    margin-bottom: 30px;
+}
+
+.m-r-0 {
+    margin-right: 0px;
+}
+
+.m-l-0 {
+    margin-left: 0px;
+}
+
+.user-card-full .user-profile {
+    border-radius: 5px 0 0 5px;
+}
+
+.bg-c-lite-green {
+        background: -webkit-gradient(linear, left top, right top, from(#5d4a8b), to(#5aee66));
+    background: linear-gradient(to right, #a3bded, #6991c7);
+}
+
+.user-profile {
+    padding: 40px 0;
+}
+
+.card-block {
+    padding: 1.25rem;
+}
+
+.m-b-25 {
+    margin-bottom: 25px;
+}
+
+.img-radius {
+    border-radius: 5px;
+}
+
+
+ 
+h6 {
+    font-size: 14px;
+}
+
+.card .card-block p {
+    line-height: 25px;
+}
+
+@media only screen and (min-width: 1400px){
+p {
+    font-size: 14px;
+}
+}
+
+.card-block {
+    padding: 1.25rem;
+}
+
+.b-b-default {
+    border-bottom: 1px solid #e0e0e0;
+}
+
+.m-b-20 {
+    margin-bottom: 20px;
+}
+
+.p-b-5 {
+    padding-bottom: 5px !important;
+}
+
+.card .card-block p {
+    line-height: 25px;
+}
+
+.m-b-10 {
+    margin-bottom: 10px;
+}
+
+.text-muted {
+    color: #919aa3 !important;
+}
+
+.b-b-default {
+    border-bottom: 1px solid #e0e0e0;
+}
+
+.f-w-600 {
+    font-weight: 600;
+}
+
+.m-b-20 {
+    margin-bottom: 20px;
+}
+
+.m-t-40 {
+    margin-top: 20px;
+}
+
+.p-b-5 {
+    padding-bottom: 5px !important;
+}
+
+.m-b-10 {
+    margin-bottom: 10px;
+}
+
+.m-t-40 {
+    margin-top: 20px;
+}
+
+.user-card-full .social-link li {
+    display: inline-block;
+}
+
+.user-card-full .social-link li a {
+    font-size: 20px;
+    margin: 0 10px 0 0;
+    -webkit-transition: all 0.3s ease-in-out;
+    transition: all 0.3s ease-in-out;
+}
+
+
+
+</style>

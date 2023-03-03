b7bd72714d65ad8c67ed4a5375c51486fd6ee208
Yufei Miao <yufeim@yufeim.com>
2023-03-03 01:40:54 -0600

    fix start scripts
    Signed-off-by: Yufei Miao <yufeim@yufeim.com>

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -8,5 +8,6 @@ sudo -u $WEB_USER python3 -m venv $PYTHON_ENV_PATH
 sudo -u $WEB_USER $PYTHON_ENV_PATH/bin/python3 -m pip install -r $APPLICATION_PATH$BACKEND_PATH_SUFFIX/requirements.txt
 cd $APPLICATION_PATH$FRONTEND_PATH_SUFFIX
 sudo -u $WEB_USER npm install
+sudo -u $WEB_USER npm run build
 
 exit 0
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -9,8 +9,8 @@ cd $APPLICATION_PATH$BACKEND_PATH_SUFFIX
 sudo -u $WEB_USER $PYTHON_ENV_PATH/bin/python3 $APPLICATION_PATH$BACKEND_PATH_SUFFIX/main.py >>$LOG_PATH/backend.log 2>&1 &
 echo $! > $PID_PATH/backend.pid
 
-cd $APPLICATION_PATH$FRONTEND_PATH_SUFFIX
-sudo -u $WEB_USER npm run build >>$LOG_PATH/frontend.log 2>&1 &
+cd $APPLICATION_PATH$FRONTEND_PATH_SUFFIX/production
+sudo -u $WEB_USER npm run start >>$LOG_PATH/frontend.log 2>&1 &
 echo $! > $PID_PATH/frontend.pid
 
 exit 0
